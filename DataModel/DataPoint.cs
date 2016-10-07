069ae228df33ea05c7766909ddd0ae0d87290b20
ChRonX <chronx@gmail.com>
2016-10-07 15:28:01 +0800

    adding json property value to lowercase the property name

--- DataModel/DataPoint.cs
+++ DataModel/DataPoint.cs
@@ -8,14 +8,22 @@
 
 using System;
 using Microsoft.Azure.Documents.Spatial;
+using Newtonsoft.Json;
 
 namespace DataModel
 {
     public class DataPoint : IDocument
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("datetime")]
         public DateTime? DateTime { get; set; }
+
+        [JsonProperty("speed")]
         public double Speed { get; set; }
+
+        [JsonProperty("location")]
         public Point Location { get; set; }
     }
 }
